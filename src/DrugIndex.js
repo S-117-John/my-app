3abb3c4d35dd960ab6a351a22a814c36ff1c91fc
S-117-John <lmz77555@gmail.com>
2020-06-30 18:31:41 +0800

    2

--- src/DrugIndex.js
+++ src/DrugIndex.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Head from "./head";
-import {Flex, WhiteSpace, WingBlank} from "antd-mobile";
-import {Button} from "antd";
+import { Flex, SegmentedControl, WhiteSpace, WingBlank} from "antd-mobile";
+import {Button, DatePicker, Input, Select} from "antd";
+
+const nowTimeStamp = Date.now();
+const now = new Date(nowTimeStamp);
+const { Option } = Select;
 
 class DrugIndex extends React.Component{
+
+    state = {
+        date: now,
+    }
+
+    onChange(date, dateString) {
+        console.log(date, dateString);
+    }
+
+    handleChange(value) {
+        console.log(`selected ${value}`);
+    }
+
     render() {
         return(
            <div style={{margin:15}}>
@@ -16,10 +33,35 @@ class DrugIndex extends React.Component{
                    <Button style={{marginRight:10}} type="primary">删除</Button>
                    <Button style={{marginRight:10}} type="primary">返回</Button>
                </div>
-
+               <WhiteSpace />
                <div>
                    <Flex>
+                       <SegmentedControl style={{width:200}} values={['长期', '临时']} />
+                       <span style={{marginLeft:20}}>开始时间：</span><DatePicker onChange={this.onChange} />
 
+                       <span style={{marginLeft:20}}>频次：</span>
+                       <Select defaultValue="lucy" style={{ width: 120 }} onChange={this.handleChange}>
+                           <Option value="jack">Jack</Option>
+                           <Option value="lucy">Lucy</Option>
+                           <Option value="disabled" disabled>
+                               Disabled
+                           </Option>
+                           <Option value="Yiminghe">yiminghe</Option>
+                       </Select>
+
+                       <span style={{marginLeft:20}}>用法：</span>
+                       <Select defaultValue="lucy" style={{ width: 120 }} onChange={this.handleChange}>
+                           <Option value="jack">Jack</Option>
+                           <Option value="lucy">Lucy</Option>
+                           <Option value="disabled" disabled>
+                               Disabled
+                           </Option>
+                           <Option value="Yiminghe">yiminghe</Option>
+                       </Select>
+
+                       <Input addonBefore="首:"  defaultValue="1" style={{width:100,marginLeft:20}}/>
+
+                       <Button style={{marginLeft:10}} type="primary">添加药品</Button>
                    </Flex>
                </div>
 
